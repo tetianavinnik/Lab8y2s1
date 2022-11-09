8b329b941759ed7c8685f91adf0aaa6e129c49fd
tetianavinnik <92581380+tetianavinnik@users.noreply.github.com>
2022-11-09 16:52:07 +0200

    Add files via upload

---
+++ src/main/java/ua/ucu/edu/apps/demo/flower/FlowerService.java
@@ -0,0 +1,26 @@
+package ua.ucu.edu.apps.demo.flower;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FlowerService {
+    @Autowired
+    private FlowerRepository flowerRepository;
+
+    public FlowerService(FlowerRepository flowerRepository) {
+        this.flowerRepository = flowerRepository;
+    }
+
+    public List<Flower> getFlowers() {
+        return flowerRepository.findAll();
+    }
+
+    public void addFlower(Flower flower) {
+        flowerRepository.save(flower);
+    }
+}
